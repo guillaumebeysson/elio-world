29d80eb4b45b52dfecda906efd097369c362becd
guillaumebeysson <guillaume.beysson@gmail.com>
2023-07-14 19:02:41 +0200

    level2

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const emojis = ["1.png", "8.png", "7.png", "4.png", "2.png", "6.png", "2.png", "3.png", "5.png", "1.png", "6.png", "3.png", "7.png", "4.png", "8.png", "5.png"];
+const emojis = ["1.png", "8.png", "7.png", "4.png", "2.png", "6.png", "2.png", "3.png", "5.png", "1.png", "6.png", "3.png", "7.png", "4.png", "8.png", "5.png", "9.png", "9.png", "10.png", "10.png", "11.png", "11.png", "12.png", "12.png", "13.png", "13.png", "14.png", "14.png", "15.png", "15.png", "16.png", "16.png", "17.png", "17.png", "18.png", "18.png"];
 
 
 // Fonction pour mélanger le tableau en utilisant l'algorithme de Fisher-Yates
